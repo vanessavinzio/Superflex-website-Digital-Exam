e7cc92b81e58bb772de3e1528de2006adfb683bc
vanessavinzio <128424806+vanessavinzio@users.noreply.github.com>
2023-07-18 23:02:33 +0200

    Add files via upload

---
+++ Superflex-website/SITO SUPERFLEX/origine ed influenze di superflexsitoweb/stuff.js
@@ -0,0 +1,151 @@
+
+var items = [
+    {
+        tag: "#superkilen",
+        description: `Superkilen è uno spazio pubblico di Copenaghen suddiviso in tre aree principali: La Piazza Rossa, la Piazza Nera e il Parco Verde. La Piazza Rossa rappresenta la vita urbana moderna con caffè, musica e sport, mentre la Piazza Nera è una piazza classica con fontane e panchine e il Parco Verde è un parco per picnic, sport e passeggiate con il cane.
+        Gli abitanti delle immediate vicinanze del parco rappresentano più di 50 nazionalità diverse. Invece di includere gli oggetti cittadini solitamente destinati ai parchi e agli spazi pubblici, SUPERFLEX ha chiesto agli abitanti della zona di nominare oggetti come panchine, cestini, alberi, parchi giochi, tombini e segnaletica di altri Paesi. Questi oggetti sono stati scelti o dal Paese di origine dell'abitante in questione, o da un altro luogo incontrato viaggiando. Gli oggetti sono stati prodotti in copia 1:1 o acquistati e trasportati sul posto.
+        Inoltre, nell'ambito di un processo che SUPERFLEX definisce di "partecipazione estrema", cinque gruppi hanno viaggiato con SUPERFLEX in Palestina, Spagna, Thailandia, Texas e Giamaica per acquisire cinque oggetti a loro scelta. Questi sono stati poi installati nel parco. In totale, il parco contiene ora più di 100 oggetti diversi provenienti da oltre 50 Paesi.`,
+        img: "img/superkilen.jpg",
+        metadata: {
+            title: "Superkilen",
+            year: "2012",
+            type: "Parco Pubblico e Parco Giochi",
+            place: "Copenaghen",
+            contributors: "Big e Topotek",
+            credits: "Press Photos",
+        }
+    },
+    {
+        tag: "#thebank",
+        description: `La Banca, situata in Bank Street a Sharjah, è un parco urbano che ospitava numerose banche. Tuttavia, poiché queste banche si sono trasferite in aree più redditizie, SUPERFLEX ha immaginato un nuovo concetto di banca per la strada, che non coinvolge il denaro.
+        La proposta suggerisce invece di considerare i ricordi e le storie collettive degli abitanti della zona come la vera ricchezza della strada. Questa moneta alternativa potrebbe essere investita in Bank Street e trasformata in oggetti tangibili. Questo approccio non convenzionale potrebbe portare a uno scambio in cui un dinosauro di metallo usato di un sobborgo del Portogallo diventa una forma di credit default swap, o un bancomat eroga un vaso di ceramica pieno d'acqua di un quartiere di Islamabad. La Banca funge da convertitore di ricordi e storie personali in una moneta urbana.
+        I residenti dell'area sono stati invitati a nominare oggetti specifici di diverse città, come panchine, alberi, parchi giochi e insegne, che avessero un significato per loro. Questi oggetti potevano essere associati a storie significative o a ricordi vaghi e sono stati scelti o dal loro Paese d'origine o da luoghi incontrati durante i loro viaggi. Gli oggetti nominati sono stati poi replicati in scala 1:1 o acquistati e trasportati sul posto.`,
+        img: "img/thebank.jpg",
+        metadata: {
+            title: "The Bank",
+            year: "2013",
+            type: "Parco pubblico e parco giochi",
+            place: "Sharjah",
+            contributors: "Schul landscape",
+            credits: "Superflex",
+        }
+    },
+    {
+        tag: "#ontwothreeswing",
+        description: `L'installazione intitolata "One Two Three Swing!" presenta una serie di altalene interconnesse collegate a un filo d'acciaio arancione. Queste altalene si estendono oltre i confini delle pareti della galleria, raggiungendo il paesaggio urbano e il mondo intero.
+        Le altalene fungono da pendoli alimentati dall'energia umana, convertendo l'energia potenziale in movimento condiviso. A differenza delle altalene tradizionali progettate per l'uso individuale, ogni altalena di questa installazione può ospitare tre persone. Quando le persone sull'altalena lavorano insieme, sfruttando la forza di gravità, arrivano gradualmente a un momento in cui la caduta si trasforma in volo e tutti si muovono in sincronia. Questo momento ludico scatena l'energia del movimento collettivo.
+        "One Two Three Swing!" invita il pubblico a esplorare il potere del gioco e il potenziale della collaborazione. Evidenzia le possibilità che si presentano quando ci impegniamo collettivamente nel movimento. L'esperienza condivisa facilitata dall'installazione può indurre alla contemplazione di temi più ampi come la democrazia, l'azione collettiva e la connettività sociale. In questo modo, le altalene create da SUPERFLEX non sono solo un'occasione di divertimento, ma un esperimento per attivare un'energia collettiva che ha il potenziale di rimodellare la traiettoria del nostro pianeta e della nostra società.
+        L'installazione è stata inizialmente concepita per la Hyundai Commission del 2017, tenutasi nella Turbine Hall della Tate Modern.`,
+        img: "img/ontwothreeswing.jpg",
+        metadata: {
+            title: "One Two Three Swing!",
+            year: "2017",
+            type: "Installazione",
+            place: "Tate Modern Turbin Hall",
+            contributors: "Hyundai Commission,KWY.studio,Nupergoz",
+            credits: "Superflex",
+        }
+    },
+    {
+        tag: "#plyacontract",
+        description: `Il progetto Play Contract mira ad affrontare la mancanza di prospettive dei bambini nell'arte pubblica, coinvolgendoli nel processo di progettazione. Il risultato è uno spazio ludico con cinque sculture circondate da un paesaggio adattato. In questo spazio, il comportamento dei visitatori è regolato da un contratto che chiede agli adulti di attenersi alle condizioni di gioco dei bambini.
+        Il progetto è iniziato con un gruppo di 121 bambini di Billund, in Danimarca, a cui è stato affidato il compito di esplorare e progettare modelli che potessero promuovere il gioco tra diverse generazioni. In collaborazione con KWY.studio, SUPERFLEX ha raccolto e sintetizzato questi modelli per creare un progetto finale, realizzato con marmo rosa. Le sculture sono composte da circa 300 pietre, ognuna con una forma unica. Queste pietre sono state volutamente lasciate grezze e non lucidate, per mostrare i segni lasciati dagli strumenti utilizzati per tagliarle. Ogni pietra porta con sé la propria storia, evocando un senso di appartenenza a un passato lontano o a un futuro ancora da venire.
+        Sulla base delle prospettive di gioco dei bambini, è stato preparato un contratto che è stato scolpito nelle sculture. All'interno dell'area delle sculture, gli adulti devono accettare di abbracciare l'assurdo e di aderire al concetto di tempo dei bambini. In questo spazio, gli orologi non hanno alcun potere e le scadenze perdono significato. Il progetto sfida l'inclinazione degli adulti a controllare ciò che li circonda, evidenziando che il gioco implica l'accoglienza delle sorprese e la rinuncia al controllo. Play Contract è un invito sia per i bambini che per gli adulti. Il progetto Play Contract intende affrontare la mancanza di prospettive dei bambini nell'arte pubblica coinvolgendoli nel processo di progettazione. Il risultato è uno spazio ludico con cinque sculture circondate da un paesaggio adattato.
+        Oltre alle sculture, CoC pubblicherà un libro su Play Contract. Il libro racconterà la storia del progetto e includerà numerose immagini che ne documentano lo sviluppo e il risultato.`,
+        img: "img/plyacontract.jpg",
+        metadata: {
+            title: "Play Contract",
+            year: "2021",
+            type: "Parco Pubblico e Parco Giochi",
+            place: "Billund",
+            contributors: "KWY.studio",
+            credits: "Superflex",
+        }
+    },
+    {
+        tag: "#moderntimesforever",
+        description: `Modern Times Forever (Stora Enso Building, Helsinki) è un'opera cinematografica che immagina il futuro dell'edificio Stora Enso nell'arco di migliaia di anni, influenzato unicamente dal passare del tempo e dalle condizioni atmosferiche. Con una durata di 240 ore, il film sfida le nozioni convenzionali di tempo, permanenza e modernità nel contesto dello spazio pubblico urbano.
+        Presentando l'immagine decadente della sede centrale di Stora Enso, l'opera solleva questioni importanti come il cambiamento climatico, la pianificazione urbana, la storia industriale della Finlandia e i cambiamenti nelle strutture economiche. L'edificio di Stora Enso, un importante punto di riferimento della città, è stato oggetto di discussione per il suo stile architettonico fin dalla sua costruzione. Conosciuto localmente come "il cubo di zucchero", l'edificio è stato progettato da Alvar Aalto e completato nel 1962. La sua facciata bianca e pulita in stile modernista ha suscitato ammirazione e critiche nel corso della sua esistenza.
+        Il film è stato proiettato inizialmente nella piazza del mercato di Helsinki, dove si trova l'attuale edificio Stora Enso, su un grande schermo LED di 40 m2 . Questo scenario ha permesso agli spettatori di osservare l'edificio originale e contemporaneamente di assistere al suo graduale decadimento sullo schermo. Il film è stato proiettato ininterrottamente per dieci giorni, consentendo al pubblico di vederlo 24 ore su 24 per tutta la durata della mostra.`,
+        img: "img/moderntimesforever.jpg",
+        metadata: {
+            title: "Modern Times Forever",
+            year: "2011",
+            type: "Installazione Video Art",
+            place: "Helsinki",
+            contributors: "IHME Contemporary Art Festival",
+            credits: "Superflex",
+        }
+    },
+    {
+        tag: "#aprevous",
+        description: `Après Vous, Le Déluge è un'opera d'arte creata appositamente per le Galeries Lafayette negli Champs Elysées, a Parigi. L'installazione consiste in sculture blu disposte in una linea tratteggiata sulle pareti dell'atrio dell'edificio. Questa linea discontinua funge da rappresentazione visiva di un confine invisibile, che simboleggia il previsto innalzamento del livello del mare nel prossimo secolo a causa dei cambiamenti climatici. L'altezza della linea corrisponde alle stime delineate nell'Accordo di Parigi del 2015 e la sua collocazione nella capitale francese è significativa, poiché è qui che l'accordo è stato firmato con il sostegno senza precedenti di 196 nazioni.
+        Tuttavia, l'opera d'arte funge anche da intervento all'interno dello spazio familiare dei grandi magazzini, uno spazio strettamente associato al consumo che contribuisce alle sfide legate al clima. La linea blu ricorda l'urgenza della crisi climatica che minaccia le fondamenta stesse della nostra realtà quotidiana. Invita gli spettatori a contemplare le conseguenze tangibili del cambiamento climatico sulla loro vita quotidiana. Après Vous, Le Déluge offre ai visitatori una rappresentazione astratta ma distintiva dell'impatto potenziale che gli scenari peggiori di cambiamento climatico potrebbero avere sull'ambiente circostante.`,
+        img: "img/aprevous.jpg",
+        metadata: {
+            title: "Après vous, le déluge",
+            year: "2019",
+            type: "Installazione",
+            place: "Galeries Lafayette Champs Elisées Parigi",
+            contributors: "KWY.studio",
+            credits: "Marco Cappelletti",
+        }
+    },
+    {
+        tag: "#asclose",
+        description: `Con l'innalzamento del livello del mare e la possibilità che le strutture umane vengano sommerse, diventa fondamentale considerare le esigenze e il benessere della vita marina nelle nostre decisioni di costruzione. Riconoscendo che i pesci potrebbero diventare i futuri occupanti dei nostri edifici, l'opera d'arte intitolata "As Close As We Get" presenta una serie di sculture che fungono sia da arte per gli esseri umani che da potenziali case per i pesci. Il design di queste sculture tiene conto del fatto che la biodiversità degli oceani prospera in strutture con un'ampia superficie. A tal fine, i blocchi modulari di "As Close As We Get" sono caratterizzati da numerosi piani piatti di dimensioni irregolari. Ogni scultura è composta da blocchi di costruzione impilati, creando opere d'arte che possono essere trasformate in infrastrutture subacquee.
+        Andando oltre la percezione umana del tempo, "As Close As We Get" utilizza materiali che hanno un rapporto diverso con il tempo geologico, dalle pietre formate naturalmente ai mattoni fatti apposta per i pesci. Alcuni di questi materiali sono stati modellati dalla Terra nel corso dei millenni, mentre altri sono stati sviluppati di recente grazie all'ingegno umano.
+        Installata nel porto di Copenhagen, "As Close As We Get" funge da esperimento, da dimora per i microrganismi e da opera d'arte.Sopra la superficie dell'acqua, i visitatori possono ammirare l'opera d'arte dalla riva, mentre sotto la superficie la vita marina trova un luogo di ritrovo. La sostenibilità dei materiali per la costruzione sottomarina è monitorata dal DTU Sustain, i cui risultati della ricerca contribuiranno al più ampio progetto Superrev di SUPERFLEX. Il progetto Superrev mira a costruire in collaborazione almeno 55 km2 di barriera corallina nelle acque danesi.
+        "As Close As We Get" è un progetto del 2022 realizzato in collaborazione con DTU Sustain e By & Havn, con il sostegno del Danish Art Council. Fa parte della ricerca in corso di SUPERFLEX, che esplora il rapporto tra gli esseri umani e le altre specie e propone un nuovo tipo di urbanistica che reimmagina il modo in cui possiamo coesistere.`,
+        img: "img/asclose.jpg",
+        metadata: {
+            title: "As Close As We Get",
+            year: "2021",
+            type: "Sculture",
+            place: "Copenaghen",
+            contributors: "TBA21-Accademy",
+            credits: "Robert Damisch",
+        }
+    },
+    {
+        tag: "#superflex",
+        description: `SUPERFLEX è stato fondato nel 1993 da Jakob Fenger, Bjørnstjerne Christiansen e Rasmus Rosengren Nielsen. Fin dall'inizio, il collettivo ha adottato un approccio inclusivo, collaborando con una serie di individui diversi, tra cui giardinieri, ingegneri e membri del pubblico. Esplorando modelli alternativi di organizzazione sociale ed economica, le opere di SUPERFLEX hanno assunto varie forme, come sistemi energetici, bevande, sculture, repliche, sedute di ipnosi, infrastrutture, dipinti, vivai, contratti e spazi pubblici.
+        Se da un lato SUPERFLEX si confronta con gli spazi espositivi tradizionali, dall'altro estende la sua pratica al di là dei luoghi fisici. Dall'apertura del loro pluripremiato progetto Superkilen nel 2011, hanno partecipato attivamente a importanti progetti di spazi pubblici. Questi sforzi spesso coinvolgono la comunità, incorporando il contributo dei residenti locali, degli specialisti e dei bambini. Andando oltre la collaborazione umana, i lavori più recenti hanno persino richiesto la partecipazione di altre specie. SUPERFLEX si impegna a sviluppare una nuova forma di urbanistica che tenga conto delle prospettive di piante e animali, con l'obiettivo di promuovere una società che abbracci la coesistenza interspecifica. Secondo SUPERFLEX, le idee migliori possono provenire da fonti inaspettate, persino da un pesce.`,
+        img: "img/superflex.jpg",
+        metadata: {
+            
+        }
+    },
+]
+
+function updatePicAndBigDescription(description, image, metadata) {
+    // Updates the big description on the right side with the element that was clicked
+    document.querySelector("#big-image").src = image;
+    // metadata
+    document.querySelector("#metadata").style.backgroundColor = 'lightgray';
+    document.querySelector("#header-metadata").textContent = 'Metadati';
+    document.querySelector("#title-header").textContent = 'Titolo: ';
+    document.querySelector("#title").textContent = metadata.title;
+    document.querySelector("#year-header").textContent = 'Anno: ';
+    document.querySelector("#year").textContent = metadata.year;
+    document.querySelector("#type-header").textContent = 'Tipo: ';
+    document.querySelector("#type").textContent = metadata.type;
+    document.querySelector("#place-header").textContent = 'Luogo: ';
+    document.querySelector("#place").textContent = metadata.place;
+    document.querySelector("#contributors-header").textContent = 'Collaboratori: ';
+    document.querySelector("#contributors").textContent = metadata.contributors;
+    document.querySelector("#credits-header").textContent = 'Credits: ';
+    document.querySelector("#credits").textContent = metadata.credits;
+    // description
+    document.querySelector("#description").style.backgroundColor = 'lightgray';
+    document.querySelector("#header-description").textContent = 'Descrizione';
+    document.querySelector("#description-field").textContent = description;
+}
+
+
+// Add the listener for the thumbnails to change big image and description
+for (let i = 0; i < items.length; i++) {
+    document.querySelector(items[i].tag).addEventListener("click", function () { 
+        updatePicAndBigDescription(items[i].description,  items[i].img, items[i].metadata);
+    });
+}
